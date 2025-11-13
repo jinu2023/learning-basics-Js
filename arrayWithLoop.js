f7f6237f4cf2db5ba66cd00c4b4b6766a81bb171
jinu2023 <jinu@aptlylabs.com>
2025-11-13 16:12:16 +0530

    Add new feature files

---
+++ arrayWithLoop.js
@@ -0,0 +1,10 @@
+// let fruits = ["Apple", "Banana", "Orange"];
+// for (let i = 0; i < fruits.length; i++) {
+//   console.log(fruits[i]);
+// }
+
+let fruits = ["Apple", "Banana", "Orange"];
+
+for (let fruit of fruits) {
+  console.log(fruit);
+}
