2e3c3fab1e07ac0054bb827e52ca0b2c315c61f1
jinu2023 <jinu@aptlylabs.com>
2025-11-14 12:24:05 +0530

    Array and ojects

---
+++ formattedString.js
@@ -0,0 +1,12 @@
+//Create an array of users and display formatted information about each user.
+
+let user = {
+  name: "Sarah Johnson",
+  age: 25,
+  city: "New York",
+  email: "sarah@gamil.com",
+  phone: 8798766566,
+  isStudent: true,
+};
+const formattedString = `Name: ${user.name}, Age: ${user.age}, Occupation: ${city.occupation}`;
+console.log(formattedString);
